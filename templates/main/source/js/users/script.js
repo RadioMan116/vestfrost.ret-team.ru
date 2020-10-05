906ba4c1cd563b55a7c7cdb907e7e9d2096b937a
RadioMan116 <noris75564@gmail.com>
2020-10-05 18:24:26 +0300

    mask off required

--- templates/main/source/js/users/script.js
+++ templates/main/source/js/users/script.js
@@ -1161,7 +1161,7 @@ $(document).ready(function () {
 			});
 			$("[title=Телефон],.js-phone_mask").attr("type", "tel");
 			$("[title=Телефон],.js-phone_mask").attr("placeholder", "Телефон");
-			$("[title=Телефон],.js-phone_mask,[title=Телефон]").prop("required", true);
+			// $("[title=Телефон],.js-phone_mask,[title=Телефон]").prop("required", true);
 			Inputmask.extendAliases({
 				"customAlias": {
 					mask: "+7 (999) 999-99-99",
@@ -1983,7 +1983,7 @@ $(window).on("load", function () {
 		setTimeout(function () {
 			console.log("inputmask");
 			$("[title=Телефон],.js-phone_mask").attr("type", "tel");
-			$("[title=Телефон],.js-phone_mask,[title=Телефон]").prop("required", true);
+			// $("[title=Телефон],.js-phone_mask,[title=Телефон]").prop("required", true);
 			$("[title=Телефон],.js-phone_mask,[title=Телефон]").val("");
 			Inputmask.extendAliases({
 				"customAlias": {
